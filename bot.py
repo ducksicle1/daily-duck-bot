69e308da1513359fcad4cd3b0b7eb278140a1d55
ducksicle1 <ducksicle1@gmail.com>
2026-08-30 11:05:46 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
 import os
 import discord
 from discord.ext import tasks
-import aiohttp
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 CHANNEL_ID = int(os.getenv("DUCK_CHANNEL_ID"))
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
+
+DUCK_IMAGE = "https://images.unsplash.com/photo-1555852095-64e0c2c63e8c"
 
 
 @client.event
@@ -26,20 +27,8 @@ async def duck_loop():
         print("Could not find the duck channel.")
         return
 
-    url = "https://random-d.uk/api/random"
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            if response.status != 200:
-                print(f"Duck API returned status {response.status}")
-                return
-
-            data = await response.json()
-
-    image_url = data["url"]
-
     embed = discord.Embed()
-    embed.set_image(url=image_url)
+    embed.set_image(url=DUCK_IMAGE)
     embed.set_footer(text="🦆 Daily Duck")
 
     await channel.send(embed=embed)
@@ -50,7 +39,5 @@ async def duck_loop():
 async def before_duck_loop():
     await client.wait_until_ready()
 
-
-client.run(TOKEN)
 
 client.run(TOKEN)
